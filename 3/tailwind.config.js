773584e509bf4f60ebed1ff177bf58db26145146
Fateme Esmaili <fateme.e7896@gmail.com>
2022-06-08 21:11:25 +0430

    add files of question 3

---
+++ 3/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            // primary: {
+            //     100: '#dbeafe',
+            //     200: '#1f7ef9',
+            // },
+            // secondary: {
+            //     100: '#f3e8ff',
+            //     200: '#9633ff',
+            // },
+            // success: {
+            //     100: '#dcfce7',
+            //     200: '#43ef7f',
+            // },
+            
+            'default': '#ffffff',
+            'primary': '#dbeafe',
+            'secondary': '#f3e8ff',
+            'success': '#dcfce7',
+            'danger': '#fee2e2',
+            'warning': '#fef9c3',
+            'info': '#e0e7ff',
+            'light': '#f9fafb',
+            'dark': '#1f2937',
+      },
+    },
+    plugins: [],
+  }
+}
